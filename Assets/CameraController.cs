033bf505fe8712b3869750424748ba35eeaa54e1
Alper Çugun <github@alper.nl>
2015-02-25 13:51:48 +0100

    added camera button that is clickable

---
+++ Assets/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (/* Input.touchCount == 1 || */ Input.GetMouseButtonDown(0)) {
+			// Touch or mouse click
+			Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector2 touchPos = new Vector2(wp.x, wp.y);
+
+			Debug.Log (touchPos);
+
+			if (this.collider2D == Physics2D.OverlapPoint(touchPos)) {
+				Debug.Log("Camera button pressed");	
+			} else {
+				Debug.Log ("Somewhere else pressed");
+			}
+		}
+	}
+}

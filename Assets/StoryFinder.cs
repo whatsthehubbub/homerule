83e4c3265b3fc67dd906618d5d317b2a750a3435
Alper Çugun <github@alper.nl>
2015-02-25 16:56:31 +0100

    implemented basic story find interaction

---
+++ Assets/StoryFinder.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoryFinder : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			// Touch or mouse click
+			Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector2 touchPos = new Vector2(wp.x, wp.y);
+			
+			Debug.Log (touchPos);
+			
+			if (this.collider2D == Physics2D.OverlapPoint(touchPos)) {
+				// Hard increment the value on the MuseumManager
+
+				GameObject.Find("Main").GetComponent<MuseumManager>().storiesFound += 1;
+
+				// TODO turn this into an event call
+			}
+		}
+	}
+}

8d3271df5bee19dab71c6fd9cd0717aec7320340
Alper Çugun <github@alper.nl>
2015-02-25 17:22:32 +0100

    created story display in office

---
+++ Assets/StoryUpdater.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StoryUpdater : MonoBehaviour {
+
+	private Text txt;
+	private MuseumManager m;
+
+	// Use this for initialization
+	void Start () {
+		m = GameObject.Find("Main").GetComponent<MuseumManager>();
+
+		txt = gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		txt.text = "Verhalen: " + m.storiesFound;
+	}
+}

5f7bb3325454d7caf4780c33aac793ac8549b1e3
hedgefield <timhengeveld@gmail.com>
2015-02-27 12:28:56 +0100

    Observatie UI prefab in plein scene

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+
+	[HideInInspector]public GameObject camera;
+
+	void Start()
+	{
+		camera = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			camera.transform.Translate(new Vector3(camera.transform.position.x-0.1f, camera.transform.position.y, camera.transform.position.z));
+		}
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			camera.transform.Translate(new Vector3(camera.transform.position.x+0.1f, camera.transform.position.y, camera.transform.position.z));
+		}
+	}
+}
---
+++ Assets/Scripts/ObservatieUI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObservatieUI : MonoBehaviour {
+
+	private bool geopend = false;
+	private bool bekeken = false;
+	public GameObject hotspot;
+	public GameObject camera;
+	public GameObject oog;
+	
+	public void OnClickHotspot()
+	{
+		if (geopend == false)
+		{
+			geopend = true;
+			oog.gameObject.GetComponent<Animator>().Play("open");
+			camera.gameObject.GetComponent<Animator>().Play("open");
+		}
+		else if (geopend == true)
+		{
+			geopend = false;
+			oog.gameObject.GetComponent<Animator>().Play("sluit");
+			camera.gameObject.GetComponent<Animator>().Play("sluit");
+		}
+
+	}
+
+	public void OnClickCamera()
+	{
+		Debug.Log("Klik!");
+		//maak foto (verhaal +1)
+	}
+
+	public void OnClickOog()
+	{
+		Debug.Log ("Interessant...");
+		//display beschrijving
+	}
+}

b28f5629ded8f01f2ca3a2beaa4c3a65fe63af1c
Alper Çugun <github@alper.nl>
2015-03-04 18:41:39 +0100

    show a correct hint on office

---
+++ Assets/PraatUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PraatUI : MonoBehaviour {
+
+	public GameObject label;
+	public Text text;
+
+	public void TalkClicked() {
+		label.SetActive(true);
+
+		MuseumManager mm = GameObject.Find("Main").GetComponent<MuseumManager>();
+
+		if (mm != null) {
+			if (mm.observationLocations.Count > 0) {
+				text.text = "Ik zou eens gaan kijken in " + mm.observationLocations[0];
+			} else {
+				text.text = "Ik heb geen informatie.";
+			}
+		}
+	}
+}

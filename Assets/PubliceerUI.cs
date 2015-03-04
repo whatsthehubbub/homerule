82e56eaa256be07916de4dc9fefc80fe8b208de1
Alper Çugun <github@alper.nl>
2015-03-04 17:35:15 +0100

    trying to add a script to the publish button

---
+++ Assets/PubliceerUI.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PubliceerUI : MonoBehaviour {
+
+	public GameObject hand;
+	public GameObject progress;
+
+	public void OnClickHand() {
+		Debug.Log ("Hand Clicked");
+	}
+
+//	public void OnClickHotspot()
+//	{
+//		if (geopend == false)
+//		{
+//			geopend = true;
+//			oog.gameObject.GetComponent<Animator>().Play("open");
+//			camera.gameObject.GetComponent<Animator>().Play("open");
+//		}
+//		else if (geopend == true)
+//		{
+//			geopend = false;
+//			oog.gameObject.GetComponent<Animator>().Play("sluit");
+//			camera.gameObject.GetComponent<Animator>().Play("sluit");
+//		}
+//	}
+//	
+//	public void OnClickCamera()
+//	{
+//		Debug.Log("Klik!");
+//		//maak foto (verhaal +1)
+//		bekeken = true;
+//		camera.SetActive(false);
+//		
+//		GameObject.Find ("Main").SendMessage("FoundObservation");
+//	}
+//	
+//	public void OnClickOog()
+//	{
+//		Debug.Log ("Interessant...");
+//		//display beschrijving
+//	}
+
+}

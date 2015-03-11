8ebba6d860bae05842796a547f31b4beff8e2028
hedgefield <timhengeveld@gmail.com>
2015-03-11 11:31:21 +0100

    Added fonts and close button to Agent UI

---
+++ Assets/Scripts/AgentUI.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AgentUI : MonoBehaviour
+{
+	public void Close()
+	{
+		Destroy(this.gameObject);
+		//en wat er code-wise moet gebeuren qua agent verplaatsen en alles
+	}
+}

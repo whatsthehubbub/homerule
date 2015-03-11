5cd1cc492c5f6204d80ca7720fe298584de5e0c8
hedgefield <timhengeveld@gmail.com>
2015-03-11 11:03:49 +0100

    Hooked up Map UI
    Had to add it manually to all scenes just like the Resources UI so they
    can intercommunicate with each other's scripts. There ought to be a
    better way but it's fine.

---
+++ Assets/Scripts/MapUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapUI : MonoBehaviour
+{	
+	private GameObject map;
+
+	void Start()
+	{
+		map = this.gameObject;
+	}
+
+	public void OpenMap()
+	{
+		this.gameObject.SetActive(true);
+	}
+
+	public void CloseMap()
+	{
+		this.gameObject.SetActive(false);
+	}
+}

2048bb8a7a6a6cd76d4f2971c8710fde32f9e856
hedgefield <timhengeveld@gmail.com>
2015-03-12 17:39:09 +0100

    Added animation to bossman

---
+++ Assets/Scripts/Talk.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Talk : MonoBehaviour
+{
+	private Animator anim;
+
+	void Start()
+	{
+		anim = this.gameObject.GetComponent<Animator>();
+	}
+	
+	public void TalkTo()
+	{
+		anim.Play("talk");
+		//hierna moet ie nog een keer terug naar idle, maar voor het idee.
+	}
+}

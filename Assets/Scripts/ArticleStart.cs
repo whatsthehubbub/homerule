ee27c409f8aba8b5bac10944f7c5603787be1970
Alper Çugun <github@alper.nl>
2015-04-06 17:12:18 +0200

    enabled camera control on write article

---
+++ Assets/Scripts/ArticleStart.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArticleStart : MonoBehaviour {
+
+
+	public GameObject cameraDisplay;
+
+	private WebCamTexture mCamera = null;
+
+	// Use this for initialization
+	void Start () {
+		mCamera = new WebCamTexture ();
+		mCamera.Play();
+
+		cameraDisplay.GetComponent<Renderer>().material.mainTexture = mCamera;
+
+//		cameraDisplay.GetComponent<SpriteRenderer>().material.mainTexture = mCamera;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void TakePhotoButton() {
+	}
+}

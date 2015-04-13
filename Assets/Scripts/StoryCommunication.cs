6b0fb17300cdb8a2f0464fbaf15ac1790061cefa
Alper Çugun <github@alper.nl>
2015-04-13 16:01:46 +0200

    enabled some scrolling on the story comms view

---
+++ Assets/Scripts/StoryCommunication.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoryCommunication : MonoBehaviour {
+
+	private GameObject reply;
+
+	// Use this for initialization
+	void Start () {
+	
+		reply = GameObject.Find ("Reply");
+		reply.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GoodButton() {
+		reply.SetActive(true);
+	}
+
+	public void BadButton() {
+		reply.SetActive(true);
+	}
+}

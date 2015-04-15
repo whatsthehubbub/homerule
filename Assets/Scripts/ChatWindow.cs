dd48f85ac634285e1d2a1953db659d317b5a6246
Alper Çugun <github@alper.nl>
2015-04-15 19:36:10 +0200

    created basic chat interface for the officer

---
+++ Assets/Scripts/ChatWindow.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChatWindow : MonoBehaviour {
+
+	public float startY = 600;
+	public float bubblePadding;
+
+	public GameObject lastAdded;
+	public bool heightDirty = false;
+
+	// Use this for initialization
+	void Start () {
+
+		this.bubblePadding = 30;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Really weird but we have to wait for the value to be calculated and that may take some time
+
+		// TODO this will go wrong if too much stuff is added at once
+		Rect rect = ((RectTransform)lastAdded.transform).rect;
+
+		if (heightDirty && rect.height > 0.0f) {
+			Debug.Log (((RectTransform)lastAdded.transform).rect);
+			
+			this.startY -= rect.height;
+
+			// Also now that we know the height, move it down a bit more to correct for very high bubbles
+			Vector2 position = lastAdded.transform.localPosition;
+			position.y -= (rect.height / 2);
+			lastAdded.transform.localPosition = position;
+
+			this.startY -= bubblePadding;
+
+			Debug.Log ("New start Y " + this.startY);
+			
+			heightDirty = false;
+		}
+	}
+
+
+	public GameObject AddPlayerBubble(string text) {
+		GameObject scrollContent = this.gameObject.transform.Find("ScrollView/ScrollContent").gameObject;
+
+		GameObject playerBubble = (GameObject)Instantiate(Resources.Load ("Prefabs/PlayerChatbubble"));
+		playerBubble.name = "New Button";
+
+		Text chatText = playerBubble.GetComponentInChildren<Text>();
+		chatText.text = text;
+
+		playerBubble.transform.parent = scrollContent.transform;
+		playerBubble.transform.localPosition = new Vector2(70.0f, startY);
+
+		this.lastAdded = playerBubble;
+		this.heightDirty = true;
+
+		return playerBubble;
+	}
+
+	// TODO code is duplicate
+	public GameObject AddNPCBubble(string text) {
+		GameObject scrollContent = this.gameObject.transform.Find("ScrollView/ScrollContent").gameObject;
+		
+		GameObject playerBubble = (GameObject)Instantiate(Resources.Load ("Prefabs/NPCChatbubble"));
+		playerBubble.name = "New Button";
+		
+		Text chatText = playerBubble.GetComponentInChildren<Text>();
+		chatText.text = text;
+		
+		playerBubble.transform.parent = scrollContent.transform;
+		playerBubble.transform.localPosition = new Vector2(-70.0f, startY);
+		
+		this.lastAdded = playerBubble;
+		this.heightDirty = true;
+		
+		return playerBubble;
+	}
+
+}
--- Assets/Scripts/IntroOfficer.cs
+++ Assets/Scripts/IntroOfficer.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class IntroOfficer : MonoBehaviour {
+
+	public GameObject chat;
+	public ChatWindow cw;
 
 	// Use this for initialization
 	void Start () {
@@ -18,5 +22,37 @@ public class IntroOfficer : MonoBehaviour {
 		if (main != null) {
 			main.SendMessage("IntroOfficerDone");
 		}
+	}
+
+	public void ShowChatButton() {
+		chat = (GameObject)Instantiate(Resources.Load ("Prefabs/Chat"));
+		chat.name = "Chat";
+
+		cw = chat.GetComponent<ChatWindow>();
+
+		GameObject bubble = cw.AddNPCBubble("Niks ernstigs. Heeft u deze persoon gezien?");
+
+		Invoke("ShowPlayerResponses", 0.5f);
+	}
+
+	public void ShowPlayerResponses() {
+		GameObject response = cw.AddPlayerBubble("Nooit gezien.");
+		response.GetComponentInChildren<Button>().onClick.AddListener(() => {ShowOfficerResponse();});
+	}
+
+	public void ShowOfficerResponse() {
+		GameObject response = cw.AddNPCBubble("Spijtig, we zouden haar graag eens spreken.");
+		Invoke ("ShowOfficerCloseOff", 0.5f);
+	}
+
+	public void ShowOfficerCloseOff() {
+		GameObject response = cw.AddNPCBubble("Als u de reporter spreekt zeg dan maar dat ze ons moet opzoeken.");
+		Invoke ("ShowPlayerCloseOff", 0.5f);
+	}
+
+	public void ShowPlayerCloseOff() {
+		GameObject response = cw.AddPlayerBubble("Ok…");
+
+		response.GetComponentInChildren<Button>().onClick.AddListener(() => { IntroDoneButton(); });
 	}
 }

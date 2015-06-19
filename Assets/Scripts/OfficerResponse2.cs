c9177df0ca34a02a7237b361ebb7040e238ff29c
Alper Çugun <github@alper.nl>
2015-06-19 16:53:40 +0200

    added code to display image in officer 2

--- Assets/Scripts/OfficerResponse2.cs
+++ Assets/Scripts/OfficerResponse2.cs
@@ -74,12 +74,12 @@ public class OfficerResponse2 : MonoBehaviour {
 		
 		// Display the story image
 		Sprite articleSprite = Resources.Load<Sprite>("Sprites/chat_artikel");
-		
-		//		GameObject imageBubble = cw.AddNPCImageBubble();
-		//		imageBubble.GetComponentInChildren<Image>().sprite = articleSprite;
-		//		GameObject imageObject = imageBubble.transform.Find ("BubbleImage").gameObject;
-		//		Image storyImage = imageObject.GetComponentInChildren<Image>();
-		//		storyImage.sprite = Sprite.Create (mm.storyImage, new Rect(0, 0, 200, 300), new Vector2(0.5f, 0.5f));
+
+		GameObject imageBubble = cw.AddNPCImageBubble();
+		imageBubble.GetComponentInChildren<Image>().sprite = articleSprite;
+		GameObject imageObject = imageBubble.transform.Find ("BubbleImage").gameObject;
+		Image storyImage = imageObject.GetComponentInChildren<Image>();
+		storyImage.sprite = Sprite.Create (mm.story2Image, new Rect(0, 0, 200, 300), new Vector2
 		
 		GameObject storyBubble = cw.AddNPCBubble(mm.story2Text);
 		storyBubble.GetComponentInChildren<Image>().sprite = articleSprite;

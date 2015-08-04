3775ff0abc919cc7fb12d06489ab409faf48c950
Alper Çugun <github@alper.nl>
2015-08-04 17:04:38 +0200

    script to be at the correct scroll position on Start()

---
+++ Assets/Scripts/ScrollTop.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScrollTop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ScrollRect rect = GetComponent<ScrollRect>();
+
+		rect.verticalNormalizedPosition = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

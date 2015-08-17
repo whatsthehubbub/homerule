2788c93e295d51db4be626bb35164249f068b8a6
Alper Çugun <github@alper.nl>
2015-08-17 18:35:42 +0200

    turn on call busy while youre choosing whether to stop playing

--- Assets/Scripts/GoalScript.cs
+++ Assets/Scripts/GoalScript.cs
@@ -2,17 +2,34 @@
 using System.Collections;
 
 public class GoalScript : MonoBehaviour {
-	
+
+	private MuseumManager mm;
+
+	public GameObject stopOverlay;
+
 	// Use this for initialization
 	void Start () {
-		
+		GameObject main = GameObject.Find("Main");
+		mm = main.GetComponentInChildren<MuseumManager>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
-	
+
+	public void ShowStopOverlay() {
+		mm.callBusy = true;
+
+		this.stopOverlay.SetActive(true);
+	}
+
+	public void HideStopOverlay() {
+		mm.callBusy = false;
+
+		this.stopOverlay.SetActive(false);
+	}
+
 	public void CloseGoal() {
 		GameObject goal = GameObject.Find("GoalOverlay");
 		

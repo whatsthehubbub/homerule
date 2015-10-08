8c508ce542bb9a8af7c6ffed92e3630d9f8b714f
Alper Çugun <github@alper.nl>
2015-10-08 16:44:53 +0200

    added coroutines and delays to artist response 1

--- Assets/Scripts/ArtistResponse1.cs
+++ Assets/Scripts/ArtistResponse1.cs
@@ -33,7 +33,7 @@ public class ArtistResponse1 : MonoBehaviour {
 			audioSource.Stop ();
 
 			GameObject.Destroy(call);
-			ShowVideoCall();
+			StartCoroutine(ShowVideoCall());
 		});
 	}
 	
@@ -42,7 +42,7 @@ public class ArtistResponse1 : MonoBehaviour {
 //		
 //	}
 
-	public void ShowVideoCall() {
+	public IEnumerator ShowVideoCall() {
 		chat = (GameObject)Instantiate(Resources.Load ("Prefabs/VideoCall"));
 		chat.transform.SetParent(GameObject.Find ("Canvas").transform, false);
 		chat.name = "VideoCall";
@@ -53,8 +53,12 @@ public class ArtistResponse1 : MonoBehaviour {
 		GameObject displayImage = GameObject.Find ("DisplayImage");
 		Sprite videoCallSprite = Resources.Load<Sprite>("Sprites/portrait kunstenaar wide");
 		displayImage.GetComponentInChildren<Image>().sprite = videoCallSprite;
-		
+
+		yield return new WaitForSeconds(0.5f);
+
 		cw.AddNPCBubble("Hoi, weet je wie ik ben?");
+
+		yield return new WaitForSeconds(0.5f);
 		
 		GameObject what = cw.AddButton ("Ja");
 		what.GetComponentInChildren<Button>().onClick.AddListener(() => {
@@ -68,11 +72,11 @@ public class ArtistResponse1 : MonoBehaviour {
 				cw.AddPlayerBubble("Jij bent die kunstenaar, toch?");
 			}
 				
-			Invoke ("ShowChat", 0.5f);
+			StartCoroutine(ShowChat());
 		});
 	}
 	
-	public void ShowChat() {
+	public IEnumerator ShowChat() {
 		GameObject.Destroy(chat);
 
 		chat = mm.artistChatHistory;
@@ -81,6 +85,8 @@ public class ArtistResponse1 : MonoBehaviour {
 		cw = chat.GetComponent<ChatWindow>();
 		cw.DisableBack();
 
+		yield return new WaitForSeconds(0.5f);
+
 		if (mm.story1OpinionDescription == Story1OpinionDescription.VANDAL) {
 			cw.AddNPCBubble("Vandaal? Even dimmen, ik ben een dichter!");
 		} else if (mm.story1OpinionDescription == Story1OpinionDescription.CITIZEN) {
@@ -88,9 +94,16 @@ public class ArtistResponse1 : MonoBehaviour {
 		} else if (mm.story1OpinionDescription == Story1OpinionDescription.ARTIST) {
 			cw.AddNPCBubble("Klopt. Ik was bijna opgepakt vanwege mijn gedichten.");
 		}
+
+		yield return new WaitForSeconds(0.5f);
 		
 		cw.AddNPCBubble("Ik weet dat ik niet op gebouwen mag schrijven. Maar soms doe ik het toch. Als ik echt iets te zeggen heb.");
+
+		yield return new WaitForSeconds(0.5f);
+
 		cw.AddNPCBubble("Ik ben trouwens Frank. Aangenaam kennis te maken.");
+
+		yield return new WaitForSeconds(0.5f);
 		
 		GameObject nice = cw.AddButton("Aangenaam");
 		nice.GetComponentInChildren<Button>().onClick.AddListener(() => {
@@ -98,12 +111,16 @@ public class ArtistResponse1 : MonoBehaviour {
 
 			cw.AddPlayerBubble("Aangenaam, Frank.");
 
-			Invoke ("ShowArtistStatement", 0.5f);
+			StartCoroutine(ShowArtistStatement());
 		});
 	}
 
-	public void ShowArtistStatement() {
+	public IEnumerator ShowArtistStatement() {
+		yield return new WaitForSeconds(0.5f);
+
 		cw.AddNPCBubble("Zeg, is je iets raars opgevallen net, met die huizen?");
+
+		yield return new WaitForSeconds(0.5f);
 
 		GameObject no = cw.AddButton("Nee");
 		no.GetComponentInChildren<Button>().onClick.AddListener(() => {
@@ -111,16 +128,22 @@ public class ArtistResponse1 : MonoBehaviour {
 			
 			cw.AddPlayerBubble("Nee, hoezo?");
 			
-			Invoke ("ShowArtistLook", 0.5f);
+			StartCoroutine(ShowArtistLook());
 		});
 	}
 
-	public void ShowArtistLook() {
+	public IEnumerator ShowArtistLook() {
+		yield return new WaitForSeconds(0.5f);
+
 		cw.AddNPCBubble("Kijk nog eens goed.");
+
+		yield return new WaitForSeconds(0.5f);
 
 		GameObject imageBubble = cw.AddNPCImageBubble();
 		Sprite homesSprite = Resources.Load<Sprite>("Sprites/S2 intro closeup");
 		imageBubble.transform.Find("Bubble/BubbleImage").GetComponentInChildren<Image>().sprite = homesSprite;
+
+		yield return new WaitForSeconds(0.5f);
 		
 		GameObject button = cw.AddButton("Vogels");
 		button.GetComponentInChildren<Button>().onClick.AddListener(() => {
@@ -128,14 +151,20 @@ public class ArtistResponse1 : MonoBehaviour {
 			
 			cw.AddPlayerBubble("Het zijn allemaal vogels!");
 			
-			Invoke ("ShowArtistSafety", 0.5f);
+			StartCoroutine(ShowArtistSafety());
 		});
 	}
 
-	public void ShowArtistSafety() {
+	public IEnumerator ShowArtistSafety() {
+		yield return new WaitForSeconds(0.5f);
+
 		cw.AddNPCBubble("Inderdaad. Wij vogels houden van vrijheid. We vinden het niks als iemand zegt wat we moeten doen.");
 
+		yield return new WaitForSeconds(0.5f);
+
 		cw.AddNPCBubble("Daarom zijn we uit onze huizen gezet.");
+
+		yield return new WaitForSeconds(0.5f);
 
 		GameObject button = cw.AddButton("Veiligheid?");
 		button.GetComponentInChildren<Button>().onClick.AddListener(() => {
@@ -143,12 +172,16 @@ public class ArtistResponse1 : MonoBehaviour {
 			
 			cw.AddPlayerBubble("Maar dat was toch vanwege de veiligheid?");
 			
-			Invoke ("ShowArtistTrouble", 0.5f);
+			StartCoroutine(ShowArtistTrouble());
 		});
 	}
 
-	public void ShowArtistTrouble() {
+	public IEnumerator ShowArtistTrouble() {
+		yield return new WaitForSeconds(0.5f);
+
 		cw.AddNPCBubble("Dat zeiden ze, maar het klopt niet. We moesten eruit omdat ze ons lastig vinden.");
+
+		yield return new WaitForSeconds(0.5f);
 		
 		GameObject button = cw.AddButton("Lastig?");
 		button.GetComponentInChildren<Button>().onClick.AddListener(() => {
@@ -156,14 +189,20 @@ public class ArtistResponse1 : MonoBehaviour {
 			
 			cw.AddPlayerBubble("Hoe bedoel je, lastig?");
 			
-			Invoke ("ShowArtistExplanation1", 0.5f);
+			StartCoroutine(ShowArtistExplanation1());
 		});
 	}
 
-	public void ShowArtistExplanation1() {
+	public IEnumerator ShowArtistExplanation1() {
+		yield return new WaitForSeconds(0.5f);
+
 		cw.AddNPCBubble("We zitten vaak buiten op de stoep, muziek te maken.");
+
+		yield return new WaitForSeconds(0.5f);
 		
 		cw.AddNPCBubble("Dat mag niet, de politie zegt dat het zorgt voor overlast. Maar wij zijn dol op onze fluitconcerten.");
+
+		yield return new WaitForSeconds(0.5f);
 		
 		GameObject button = cw.AddButton("Lastig!");
 		button.GetComponentInChildren<Button>().onClick.AddListener(() => {
@@ -171,14 +210,20 @@ public class ArtistResponse1 : MonoBehaviour {
 			
 			cw.AddPlayerBubble("Ik kan me voorstellen dat ze dat lastig vinden!");
 
-			Invoke ("ShowArtistExplanation2", 0.5f);
+			StartCoroutine(ShowArtistExplanation2());
 		});
 	}
 
-	public void ShowArtistExplanation2() {
+	public IEnumerator ShowArtistExplanation2() {
+		yield return new WaitForSeconds(0.5f);
+
 		cw.AddNPCBubble("Lastig of niet, ze moeten niet liegen. Vrije vogels vinden dat verschrikkelijk!");
 
+		yield return new WaitForSeconds(0.5f);
+
 		cw.AddNPCBubble("Maar ik bel om je iets te vragen.");
+
+		yield return new WaitForSeconds(0.5f);
 		
 		GameObject button = cw.AddButton("Wat?");
 		button.GetComponentInChildren<Button>().onClick.AddListener(() => {
@@ -186,12 +231,16 @@ public class ArtistResponse1 : MonoBehaviour {
 			
 			cw.AddPlayerBubble("Wat wil je vragen?");
 			
-			Invoke ("ShowArtistQuestion", 0.5f);
+			StartCoroutine(ShowArtistQuestion());
 		});
 	}
 
-	public void ShowArtistQuestion() {
+	public IEnumerator ShowArtistQuestion() {
+		yield return new WaitForSeconds(0.5f);
+
 		cw.AddNPCBubble("Jij kent toch die verslaggever? Wil je me helpen dit verhaal naar buiten te brengen?");
+
+		yield return new WaitForSeconds(0.5f);
 
 		GameObject scared = cw.AddButton("Durf niet");
 		scared.GetComponentInChildren<Button>().onClick.AddListener(() => {
@@ -201,7 +250,7 @@ public class ArtistResponse1 : MonoBehaviour {
 
 			cw.AddPlayerBubble("Ik durf het niet.");
 			
-			Invoke ("ShowArtistResponse", 0.5f);
+			StartCoroutine(ShowArtistResponse());
 		});
 
 		GameObject dunno = cw.AddButton("Weet niet");
@@ -212,7 +261,7 @@ public class ArtistResponse1 : MonoBehaviour {
 
 			cw.AddPlayerBubble("Ik weet het nog niet.");
 			
-			Invoke ("ShowArtistResponse", 0.5f);
+			StartCoroutine(ShowArtistResponse());
 		});
 
 		GameObject good = cw.AddButton("Oké");
@@ -223,24 +272,22 @@ public class ArtistResponse1 : MonoBehaviour {
 
 			cw.AddPlayerBubble("Oké. Ik doe het.");
 			
-			Invoke ("ShowArtistResponse", 0.5f);
+			StartCoroutine(ShowArtistResponse());
 		});
 	}
 
-	public void ShowArtistResponse() {
+	public IEnumerator ShowArtistResponse() {
+		yield return new WaitForSeconds(0.5f);
+
 		if (mm.artist1Answer == Artist1Answer.SCARED) {
 			cw.AddNPCBubble("Ik begrijp het. De politie houdt je in de gaten, het is spannend. Maar dit is zo belangrijk. Wees moedig!");
-
 		} else if (mm.artist1Answer == Artist1Answer.DUNNO) {
 			cw.AddNPCBubble("Ik begrijp het. De politie houdt je in de gaten, het is spannend. Maar dit is zo belangrijk. Wees moedig!");
 		} else if (mm.artist1Answer == Artist1Answer.CONFIRM) {
 			cw.AddNPCBubble("Wat fijn! Kun je haar vertellen wat er aan de hand is?");
 		}
 
-		Invoke ("ShowConclusion", 0.5f);
-	}
-
-	public void ShowConclusion() {
+		yield return new WaitForSeconds(0.5f);
 
 		GameObject button = cw.AddButton("Oké");
 		button.GetComponentInChildren<Button>().onClick.AddListener(() => {
@@ -248,27 +295,32 @@ public class ArtistResponse1 : MonoBehaviour {
 			
 			cw.AddPlayerBubble("Oké…");
 
-			cw.AddNPCBubble("Het is schandalig hoe ze ons behandelen. Wij zijn altijd de gebeten honden! Dat moet stoppen.");
-			cw.AddNPCBubble("Zeg, ik voel een gedicht opkomen. Ik hang op.");
-
-			
-			Invoke ("ShowClose", 0.5f);
+			StartCoroutine(ShowConclusion());
 		});
 	}
-	
-	public void ShowClose() {
-		
+
+	public IEnumerator ShowConclusion() {
+		yield return new WaitForSeconds(0.5f);
+
+		cw.AddNPCBubble("Het is schandalig hoe ze ons behandelen. Wij zijn altijd de gebeten honden! Dat moet stoppen.");
+
+		yield return new WaitForSeconds(0.5f);
+
+		cw.AddNPCBubble("Zeg, ik voel een gedicht opkomen. Ik hang op.");
+
+		yield return new WaitForSeconds(0.5f);
+
 		GameObject button = cw.AddButton ("Succes");
 		button.GetComponentInChildren<Button>().onClick.AddListener(() => {
 			cw.ClearButtons();
-
+			
 			cw.AddPlayerBubble("Succes ermee. Tot later!");
 			
 			cw.EnableBack();
 			chat.SetActive(false);
 			
 			mm.callBusy = false;
-
+			
 			GameObject.Destroy(this);
 		});
 	}

c7a604492fde755e187a2f37a9f3f65a3a35bad0
Alper Çugun <github@alper.nl>
2015-10-08 12:25:48 +0200

    added a comment about the exact order of the stories being processed

--- Assets/Scripts/MuseumManager.cs
+++ Assets/Scripts/MuseumManager.cs
@@ -112,6 +112,20 @@ public class MuseumManager : MonoBehaviour {
 
 	public bool forceCalls = false;
 
+	/*
+	 * The stories will be processed in this order (some of them depending on a beacon being present.
+	 * ReporterStory0
+	 * ReporterStory1
+	 * OfficerResponse1
+	 * ReporterResponse1
+	 * ReporterStory2
+	 * OfficerResponse2
+	 * ArtistResponse1
+	 * ReporterResponse2
+	 * ReporterStory3
+	 * OfficerResponse3
+	 * ReporterStory4
+	 */
 	public Queue<string> storyQueue = new Queue<string>();
 	public bool waitingForCall = false;
 	public bool callBusy = false;

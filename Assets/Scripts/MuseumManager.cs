5e89a90d12e8c42ab6f787af6ed697f45ce0b158
Alper Çugun <github@alper.nl>
2015-10-08 18:18:03 +0200

    comment out testing code

--- Assets/Scripts/MuseumManager.cs
+++ Assets/Scripts/MuseumManager.cs
@@ -499,10 +499,10 @@ public class MuseumManager : MonoBehaviour {
 
 			float delay = callDelays[this.storyQueue.Peek ()];
 
-			if (Application.platform == RuntimePlatform.OSXEditor) {
-				// Remove this for testing on desktop
-				delay = 5.0f;
-			}
+//			if (Application.platform == RuntimePlatform.OSXEditor) {
+//				// Remove this for testing on desktop
+//				delay = 5.0f;
+//			}
 
 			Debug.Log ("Going to take a call in: " + delay);
 

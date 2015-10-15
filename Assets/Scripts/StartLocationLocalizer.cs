9a4b5192154af8389654c523faba4ebcdaea156b
Alper Çugun <github@alper.nl>
2015-10-15 14:47:42 +0200

    remove todo

--- Assets/Scripts/StartLocationLocalizer.cs
+++ Assets/Scripts/StartLocationLocalizer.cs
@@ -12,8 +12,6 @@ public class StartLocationLocalizer : MonoBehaviour {
 		GameObject.Find("LocationScopeText").GetComponent<Text>().text = mm.museum.museumScope;
 
 		GameObject.Find ("StartLocationImage").GetComponentInChildren<Image>().sprite = Resources.Load<Sprite>("Sprites/Locaties/" + mm.museum.GetStartGoal().locationSprite);
-
-		// TODO also change the image of where you have to go
 	}
 	
 	// Update is called once per frame

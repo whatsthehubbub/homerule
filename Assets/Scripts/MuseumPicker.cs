5c3e898cda77db549c3f3fdec3f737507f2b6a65
Alper Çugun <github@alper.nl>
2015-10-16 18:42:09 +0200

    hooked up museum pick button

---
+++ Assets/Scripts/MuseumPicker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MuseumPicker : MonoBehaviour {
+
+	public GameObject titleScreen;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void MuseumPicked() {
+		this.titleScreen.SetActive(true);
+		this.gameObject.SetActive(false);
+	}
+}

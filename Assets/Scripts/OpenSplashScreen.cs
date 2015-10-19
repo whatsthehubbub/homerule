af4a9a6aaef1782bd0e0077fd8cc4f9d5e183027
Alper Çugun <github@alper.nl>
2015-10-19 16:55:39 +0200

    added button to main screen to let you switch museums

---
+++ Assets/Scripts/OpenSplashScreen.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OpenSplashScreen : MonoBehaviour {
+
+	public MuseumManager mm;
+
+	public GameObject changeMuseumButton;
+
+	public GameObject museumPicker;
+
+	// Use this for initialization
+	void Start () {
+		GameObject main = GameObject.Find("Main");
+		mm = main.GetComponentInChildren<MuseumManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		changeMuseumButton.GetComponentInChildren<Text>().text = "je speelt nu in " + mm.museum.museumName;
+	}
+
+	public void ChangeMuseumButtonPressed() {
+		GameObject.Find("MuseumChoiceHolder").GetComponentInChildren<MuseumChoice>().pickMuseum = true;
+
+		this.gameObject.SetActive(false);
+		this.museumPicker.SetActive(true);
+	}
+}
